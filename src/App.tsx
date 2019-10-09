368cee40b58647edd54d0fd448135a48db353a20
Mahmoud Kahack <mahmoud.kahack@tastytrade.com>
2019-10-09 18:09:50 -0500

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ServingForm from './serving-form';
+import WelcomePage from './welcome-page';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <WelcomePage />
         <ServingForm/>
       </header>
     </div>
