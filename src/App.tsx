2ad7ee8f3ae9806ae07b690b5e86788d49b237ab
Mahmoud Kahack <mahmoud.kahack@tastytrade.com>
2019-10-15 19:38:38 -0500

    adding navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,37 @@ import * as React from 'react';
 import './App.css';
 import ServingForm from './serving-form';
 import WelcomePage from './welcome-page';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  RouteComponentProps
+} from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <WelcomePage />
-        <ServingForm/>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Welcome</Link>
+            </li>
+            <li>
+              <Link to="/serving">Enter Serving</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={WelcomePage} />
+        <Route path="/serving" exact component={ServingForm}/>
+      </div>
+    </Router>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <WelcomePage />
+    //     <ServingForm/>
+    //   </header>
+    // </div>
   );
 }
 
